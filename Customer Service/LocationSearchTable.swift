ada1dd8dd9ea4dd488a1072cbe0758497d6d00e0
masterUNG <phrombutr@gmail.com>
2017-05-05 14:04:25 +0700

    LocationSearchTable Step1

--- Customer Service/LocationSearchTable.swift
+++ Customer Service/LocationSearchTable.swift
@@ -12,6 +12,8 @@ class LocationSearchTable: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("LocatinSearchTable OK")
 
         // Do any additional setup after loading the view.
     }
@@ -22,14 +24,6 @@ class LocationSearchTable: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
