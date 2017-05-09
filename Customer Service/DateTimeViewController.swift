350767a6a1ed77d451c18ae5b14af52efc5e7914
masterUNG <phrombutr@gmail.com>
2017-05-09 09:27:57 +0700

    Create DateTimeViewController oK

---
+++ Customer Service/DateTimeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DateTimeViewController.swift
+//  Customer Service
+//
+//  Created by masterUNG on 5/9/2560 BE.
+//  Copyright © 2560 EWTC. All rights reserved.
+//
+
+import UIKit
+
+class DateTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
